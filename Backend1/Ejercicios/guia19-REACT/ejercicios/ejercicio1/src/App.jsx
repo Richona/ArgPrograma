f3281b782eb27dd500aabae6bb5c6fbc8d037b2a
richona <richardcuevas2018@gmail.com>
2023-09-19 20:42:03 -0300

    add part guide 16

---
+++ Backend1/Ejercicios/guia19-REACT/ejercicios/ejercicio1/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import ComponentFuncional from './components/ComponentFuncional'
+
+function App() {
+  /* Crear un proyecto compuesto de un solo Functional Component. En dicho componente
+mostrar al menos dos datos, como por ejemplo titulo y subtitulo.
+El componente debe ser llamado desde App, a continuación, se propondrá la jerarquía
+del árbol de componentes y de como es el llamado desde index.js
+• Index.js
+o App
+§ Ejemplo */
+
+
+  return (
+    <>
+      <ComponentFuncional/>
+    </>
+  )
+}
+
+export default App
---
+++ Backend1/Ejercicios/guia19-REACT/ejercicios/ejercicio1/src/components/ComponentFuncional.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ComponentFuncional = () => {
+    const titulo = "Hola";
+    const subtitulo = "Chau pa";
+
+  return (
+    <div>
+        <h1>{titulo}</h1>
+        <h2>{subtitulo}</h2>
+    </div>
+  )
+}
+
+export default ComponentFuncional
